86e089bb5d49f535eadf6d679ef74706f07b8ea2
Andreas <s333770@oslomet.no>
2020-02-10 14:50:08 +0100

    Some minor changes

--- src/Klient.java
+++ src/Klient.java
@@ -9,26 +9,26 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Klient {
-    private static String SERVER_ID="127.0.0.1";
+    private static String SERVER_IP="127.0.0.1";
     private static int SERVER_PORT=5050;
 
     public static void main(String[] args) throws IOException {
-        Socket socket = new Socket(SERVER_ID, SERVER_PORT); //Lager tilbkobling til server
+        Socket socket = new Socket(SERVER_IP, SERVER_PORT); //Lager tilbkobling til server
 
-        BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        BufferedReader inputTastatur = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter utFraKlient = new PrintWriter(socket.getOutputStream(), true);
+        BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));// data fra server
+        BufferedReader inputTastatur = new BufferedReader(new InputStreamReader(System.in));// input tastatur
+        PrintWriter utFraKlient = new PrintWriter(socket.getOutputStream(), true); //Data som skal sender
 
-        System.out.println("Skriv inn en nettadresse");
-        String meldingTilServer = inputTastatur.readLine();
-        utFraKlient.println(meldingTilServer);
+        System.out.println("Skriv inn en nettadresse"); // Spør bruker om å sende inn en nettadresse
+        String meldingTilServer = inputTastatur.readLine();// Melding fra bruker til server
+        utFraKlient.println(meldingTilServer); // Skriver dataen til socket som skal sendes til serveren
 
 
-        String serverResponse = input.readLine();
-        System.out.println("Retur fra server:  " + serverResponse);
+        String serverResponse = input.readLine(); // Mottar så responsen fra server
+        System.out.println("Retur fra server:  " + serverResponse); // Printer ut data for prdens skyld
 
         /////////////////////////////////////////////////////////////////////////////////////////////
-        while (true) {
+        while (true) { // Setter i en evig ventende løkke som venter på en respone fra server
             if (serverResponse == null) {
                 System.out.println("!!!Server couldn’t find the web page!!");
                 break;
@@ -46,7 +46,7 @@ public class Klient {
                 break;
             }
         }
-        socket.close();
+        socket.close(); // Slutter tilkoblingen
         System.exit(0);
 
     }
--- src/KlientParalell.java
+++ src/KlientParalell.java
@@ -4,23 +4,23 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-public class KlientParalell implements Runnable {
-    private Socket klient;
-    private BufferedReader in;
-    private PrintWriter ut;
+public class KlientParalell implements Runnable { //Implementerer runnable, som tillater tråder
+    private Socket klient; // Lager en socket klient
+    private BufferedReader in; //Reader for å lese inn datea
+    private PrintWriter ut; //printwriter
 
     public KlientParalell(Socket klientSocket) throws IOException {
-        this.klient=klientSocket;
-        in=new BufferedReader(new InputStreamReader(klient.getInputStream()));
-        ut=new PrintWriter(klient.getOutputStream(),true);
+        this.klient=klientSocket; //Oppretter ny klient på klienSocketen
+        in=new BufferedReader(new InputStreamReader(klient.getInputStream())); //reader
+        ut=new PrintWriter(klient.getOutputStream(),true); // printwriter
     }
 
 
     @Override
-    public void run() {
+    public void run() { // Run metode for paralell tilkobling av
 
-        String lestInnString = null;
-        String responsfraServer=null;
+        String lestInnString = null; // Lest inn string fra klient
+        String responsfraServer=null; // Responsen fra server
         try {
                 lestInnString = in.readLine();
                 responsfraServer= Funksjoner.seEtterWebside(lestInnString);
